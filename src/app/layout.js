bdc5cd9b8e760c0167ce9a200970c26d5ef456c3
RJPBIO <a315174@uach.mx>
2026-04-14 22:29:40 -0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,8 +25,12 @@ export default function RootLayout({ children }) {
       <head>
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="msapplication-TileColor" content="#059669" />
+        <meta name="msapplication-tap-highlight" content="no" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icon-192.png" />
+        <link rel="apple-touch-icon" sizes="512x512" href="/icon-512.png" />
       </head>
       <body className={manrope.className} style={{ margin: 0, padding: 0, overscrollBehavior: "none" }}>
         {children}
